2f8035e2c0532d07e1c9f1d66a743ca806f95098
dashieze@gmail.com <dashieze@gmail.com>
2025-02-18 22:00:23 +0000

    Edited Main and ServerModule1, testing spcay features

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -30,12 +30,7 @@ def add_order(charge_id, cart_items):
 @anvil.server.callable
 def procces_words(text):
   # Process whole documents
-  text = ("When Sebastian Thrun started working on self-driving cars at "
-        "Google in 2007, few people outside of the company took him "
-        "seriously. “I can tell you very senior CEOs of major American "
-        "car companies would shake my hand and turn away because I wasn’t "
-        "worth talking to,” said Thrun, in an interview with Recode earlier "
-        "this week.")
+  
   doc = nlp(text)
   print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
   print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
