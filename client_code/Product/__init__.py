3f731b3f9a5a0afe97588c99ffb049bf28e2668e
grace.bruketa@edu.sait.ca <grace.bruketa@edu.sait.ca>
2025-02-25 16:32:34 +0000

    Edited About, AddToCart, Cart, Cart.ItemTemplate1 and 7 more

--- client_code/Product/__init__.py
+++ client_code/Product/__init__.py
@@ -1,12 +1,12 @@
 from ._anvil_designer import ProductTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import stripe.checkout
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 from ..AddToCart import AddToCart
 
 class Product(ProductTemplate):
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,9 +1,9 @@
-import anvil.stripe
-import anvil.google.auth, anvil.google.drive, anvil.google.mail
-from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.stripe
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
 import anvil.server
 from datetime import datetime
 import stripe
