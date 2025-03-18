77a465c100fcd661b0376fbaea01c973bbab0612
grace.bruketa@edu.sait.ca <grace.bruketa@edu.sait.ca>
2025-03-18 16:46:32 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -5,8 +5,6 @@ from ..Cart import Cart
 from ..Shop import Shop
 
 
-
-
 class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -14,9 +12,11 @@ class Home(HomeTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def text_box_1_pressed_enter(self, **event_args):
+  def search_box_2_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    self.navigate(self.home_link, Home)
+    from anvil import anvil 
+    search = self.search_box_2.text
+    anvil.server.call('procces_words',search)
 
   def main_button_click(self, **event_args):
     """This method is called when the button is clicked"""
