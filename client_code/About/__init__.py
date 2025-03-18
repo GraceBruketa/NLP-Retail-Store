5f9de72fabea1bb71aae5062451c37bc89afd5f3
grace.bruketa@edu.sait.ca <grace.bruketa@edu.sait.ca>
2025-03-18 15:33:48 +0000

    FIxed UI

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -1,16 +1,36 @@
 from ._anvil_designer import AboutTemplate
 from anvil import *
-import stripe.checkout
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+# from ..Home import Home
+
 
 class About(AboutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run when the form opens.
+    # Any code you write here will run before the form opens.
+
+  def main_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    from ..Home import Home
+    home = Home()
+    open_form(home)
+
+  def home_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    from ..Home import Home
+    home = Home()
+    open_form(home)
+  
+  def shop_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    from ..Shop import Shop
+    shop = Shop()
+    open_form(shop)
+
+  def cart_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    from ..Cart import Cart
+    cart = Cart()
+    open_form(cart)
+
