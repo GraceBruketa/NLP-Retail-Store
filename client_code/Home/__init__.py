bae9d49eb5c6b78e21de7214044f985caf9eeeda
grace.bruketa@edu.sait.ca <grace.bruketa@edu.sait.ca>
2025-03-18 15:18:14 +0000

    Edited Cart, Shop, About and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -24,8 +24,7 @@ class Home(HomeTemplate):
     open_form(home)
 
   def home_button_click(self, **event_args):
-  def home_button_click(self, **event_args):
-  def home_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
     """This method is called when the button is clicked"""
     home = Home()
     open_form(home)
